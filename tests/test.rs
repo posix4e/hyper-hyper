deabc415678732257214f6d5763ab677cd528502
Alex Newman <posix4e@gmail.com>
2015-05-25 23:03:46 -0700

    added some basic tests

--- tests/test.rs
+++ tests/test.rs
@@ -9,7 +9,8 @@ use hyperhyper::HttpAction;
 
 #[test]
 fn get_a_simple_webpage() {
-    poke_web_page("news.ycombinator.com".to_string(), 
+    let result:Vec<u8> = poke_web_page("google.com".to_string(), 
     	80, 
     	HttpAction::Get(Rc::new(String::from_str("/"))));
+	assert!(String::from_utf8(result).unwrap().contains("google"));
 }
