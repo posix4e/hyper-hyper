017e9c31f240b70e670a6bbf0393cedddc1e3de1
Alex Newman <posix4e@gmail.com>
2015-05-27 00:31:19 -0700

    push with async tests

--- src/action.rs
+++ src/action.rs
@@ -8,9 +8,9 @@ use std::collections::VecMap;
 use url::Url;
 use eventual;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum HttpAction {
-    Get(Arc<String>),
+    Get(Arc<Url>),
 }
 
 pub struct Echo {
@@ -19,7 +19,7 @@ pub struct Echo {
     sender: VecMap<eventual::Complete<Box<Vec<u8>>, &'static str>>,
     mut_buf: VecMap<Vec<u8>>,
     buf: Option<ByteBuf>,
-    interest: Interest
+    interest: VecMap<Interest>
 }
 
 impl Echo {
@@ -29,7 +29,7 @@ impl Echo {
             action: VecMap::new(),
             sender: VecMap::new(),
             mut_buf: VecMap::new(),
-            interest: Interest::hup(),
+            interest: VecMap::new(),
             buf: None,
         }
     }
@@ -39,48 +39,56 @@ impl Handler for Echo {
     type Timeout = usize;
     type Message = (String, eventual::Complete<Box<Vec<u8>>, &'static str>);
 
-    fn readable(&mut self, event_loop: &mut EventLoop<Echo>, token: Token, _: ReadHint) {
-        let mut buf = ByteBuf::mut_with_capacity(2048);
+    fn readable(&mut self, event_loop: &mut EventLoop<Echo>, token: Token, hint: ReadHint) {
+        println!("Read");
+        
+        let mut buf = ByteBuf::mut_with_capacity(4096 * 16);
 
         let ref mut non_block_client = match self.non_block_client.get_mut(&token.as_usize()) {
             Some(client) => client,
             None => panic!("Error finding the associated non blocking client {:?}", token)
         };
-
+        let ref mut interest = match self.interest.get_mut(&token.as_usize()) {
+            Some(interest) => interest,
+            None => panic!("Error finding the associated interest {:?}", token)
+        };
         match non_block_client.read(&mut buf) {
+
             Ok(None) => {
                 panic!("We just got readable, but were unable to read from the socket?");
             }
             Ok(Some(r)) => {
-                self.interest.remove(Interest::readable());
+                interest.remove(Interest::readable());
                 if r == 0 {
-
+                	println!("DONE");
+                    event_loop.deregister(*non_block_client).unwrap();
                     let mut_buf = match self.mut_buf.remove(&token.as_usize()) {
                         Some(mut_buf) => mut_buf,
                         None =>
-                            panic!("Error finding the mut_buf for {:?}", token)
+                            panic!("Error finding the mut_buf for {:?} {:?}", token, self.mut_buf)
                     };
-                    
+
                     match self.sender.remove(&token.as_usize()) {
-                        Some(completer) => completer.complete(Box::new(mut_buf)),
-                        None =>
-                            panic!("Error finding the mut_buf for {:?}", token)
+                        Some(completer) => {
+                            completer.complete(Box::new(mut_buf));
+                            interest.remove(Interest::none());
+                        }
+                        None => panic!("Error finding the mut_buf for {:?}", token)
                     };
-					
-						
+
                 } else {
-                    let ref mut mut_buf = match self.mut_buf.get_mut(&token.as_usize()) {
-                        Some(mut_buf) => mut_buf,
-                        None =>
-                            panic!("Error finding the mut_buf for {:?}", token)
-                    };
-					mut_buf.push_all(buf.flip().bytes());
-                    event_loop.reregister(*non_block_client, token, self.interest,
-                                          PollOpt::edge() | PollOpt::oneshot()).unwrap();
+        event_loop.reregister(*non_block_client, token, **interest,
+                              PollOpt::edge() | PollOpt::oneshot()).unwrap();
+ 
+                    match self.mut_buf.get_mut(&token.as_usize()) {
+                        Some(mut_buf) => mut_buf.push_all(buf.flip().bytes()),
+                        None => panic!("Error finding the mut_buf for {:?}", token)
+                    }
+                    
+                    interest.insert(Interest::readable());
                 }
             }
             Err(e) => {
-                self.interest.remove(Interest::readable());
                 panic!("not implemented; client err={:?}", e);
             }
         }
@@ -91,56 +99,74 @@ impl Handler for Echo {
             Some(action) => action,
             None => panic!("Error finding the associated action {:?}", token)
         };
+        let get_command: String = body(action.clone());
 
         let ref mut non_block_client = match self.non_block_client.get_mut(&token.as_usize()) {
             Some(client) => client,
             None => panic!("Error finding the associated non blocking client {:?}", token)
         };
 
+        let ref mut interest = match self.interest.get_mut(&token.as_usize()) {
+            Some(interest) => interest,
+            None => panic!("Error finding the associated interest {:?}", token)
+        };
+
         match **action {
-            HttpAction::Get(ref resource) => {
-                let get_command: String = String::from_str("GET ") + resource + "\n";
+            HttpAction::Get(_) => {
                 let mut buf = ByteBuf::from_slice(get_command.as_bytes());
                 match non_block_client.write(&mut buf) {
                     Ok(None) => {
                         println!("client flushing buf; WOULDBLOCK");
                         self.buf = Some(buf);
-                        self.interest.insert(Interest::writable());
+                        interest.insert(Interest::writable());
                     }
                     Ok(Some(_)) => {
-                        self.interest.insert(Interest::readable());
-                        self.interest.remove(Interest::writable());
+                        interest.insert(Interest::readable());
+                        interest.remove(Interest::writable());
+                        event_loop.reregister(*non_block_client, token, **interest,
+                                              PollOpt::edge() | PollOpt::oneshot()).unwrap();
                     }
                     Err(e) => panic!("not implemented; client err={:?}", e),
                 }
-                event_loop.reregister(*non_block_client, token, self.interest,
-                                      PollOpt::edge() | PollOpt::oneshot()).unwrap();
             }
         }
     }
     fn notify(&mut self,
               event_loop: &mut EventLoop<Echo>,
               tuple: (String, eventual::Complete<Box<Vec<u8>>, &'static str>)) {
-
         let token = Token(self.action.len() + 1);
-        let url_tuple = url_tuple(tuple.0);
-        let ip = lookup_host(&url_tuple.0).unwrap().next().unwrap().unwrap();
-        let address = SocketAddr::new(ip.ip(), url_tuple.1);
-        let (sock, _) = TcpSocket::v4().unwrap().connect(&address).unwrap();
-        self.non_block_client.insert(token.as_usize(), sock);
-        self.action.insert(token.as_usize(), url_tuple.2);
-        self.sender.insert(token.as_usize(), tuple.1);
-        self.mut_buf.insert(token.as_usize(), Vec::new());
-        event_loop.register_opt(self.non_block_client.get(&token.as_usize()).unwrap(), token,
-                                Interest::writable(), PollOpt::edge() | PollOpt::oneshot())
-                .unwrap();
+        let action = get_action(tuple.0);
+        self.action.insert(token.as_usize(), action.clone());
+               
+        match action {
+            HttpAction::Get(url_p) => {
+                let url: Url = (*url_p).clone();
+                let ip = lookup_host(url.domain().unwrap()).unwrap().next().unwrap().unwrap();
+                let port = url.port_or_default().unwrap();
+                let address = SocketAddr::new(ip.ip(), port);
+                let (sock, _) = TcpSocket::v4().unwrap().connect(&address).unwrap();
+                self.non_block_client.insert(token.as_usize(), sock);
+                self.sender.insert(token.as_usize(), tuple.1);
+                self.mut_buf.insert(token.as_usize(), Vec::new());
+                self.interest.insert(token.as_usize(), Interest::hup());
+                event_loop.register_opt(self.non_block_client.get(&token.as_usize()).unwrap(),
+                                        token, Interest::writable(),
+                                        PollOpt::edge() | PollOpt::oneshot()).unwrap();
+            }
+        }
     }
 }
 
-fn url_tuple(url_s: String) -> (String, u16, HttpAction) {
-	let url = Url::parse(url_s.as_str()).unwrap();
-	let path = url.serialize_path().unwrap();
-    (url.domain().unwrap().to_string(), 
-    	url.port_or_default().unwrap(), 
-    	HttpAction::Get(Arc::new(path)))
+fn body(action: HttpAction) -> String {
+    match action {
+        HttpAction::Get(ref resource) => {
+            format!(
+                "GET {}  HTTP/1.1\r\nHost: {}\r\nUser-Agent: curl/7.37.1\r\nAccept */*\r\n\r\n",
+                resource.serialize_path().unwrap(), resource.domain().unwrap())
+        }
+    }
+}
+fn get_action(url_s: String) -> HttpAction {
+    let url = Url::parse(url_s.as_str()).unwrap();
+    HttpAction::Get(Arc::new(url))
 }
--- tests/test.rs
+++ tests/test.rs
@@ -12,20 +12,28 @@ use std::str;
 #[test]
 fn get_a_simple_webpage_2() {
     let mut event_loop = EventLoop::new().unwrap();
-    let (tx, rx) = eventual::Future::<Box<Vec<u8>>, &'static str>::pair();
+    let (tx_google, rx_google) = eventual::Future::<Box<Vec<u8>>, &'static str>::pair();
+    let (tx_reddit, rx_reddit) = eventual::Future::<Box<Vec<u8>>, &'static str>::pair();
 
     //  poke_web_page_async(event_loop,
     // 	tx,
     //  	"google.com".to_string(), 
     //   	80, 
     //    	HttpAction::Get(Rc::new(String::from_str("/"))));
-    event_loop.channel().send(("http://www.google.com/".to_string(), tx)).unwrap();
+    event_loop.channel().send(("http://www.google.com/".to_string(), tx_google)).unwrap();
+	event_loop.channel().send(("http://www.reddit.com/".to_string(), tx_reddit)).unwrap();
 
     thread::spawn(move || {
         let echo = &mut Echo::new();
         event_loop.run(echo).unwrap();
     });
 
-    let vec = &*rx.await().unwrap();
-    assert!( str::from_utf8(vec).unwrap().contains("google"));
+	println!("Getting google");
+	let google = &*rx_google.await().unwrap();
+	println!("Got google");
+	let reddit = &*rx_reddit.await().unwrap();
+
+	assert!(str::from_utf8(google).unwrap().contains("google"));
+	assert!(str::from_utf8(reddit).unwrap().contains("reddit"));
+	
 }
